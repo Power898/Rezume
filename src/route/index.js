8fb86323857d3b612f93b64edecfc2399ec1f9c0
Power898 <limbo22city@gmail.com>
2023-03-20 03:50:11 +0200

    person2

--- src/route/index.js
+++ src/route/index.js
@@ -461,3 +461,107 @@ router.get('/person', function (req, res) {
     },
   })
 })
+
+
+//==========================================
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bio', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('bio', {
+
+    layout: 'bio',
+
+    page: {
+      title: 'Resume/Bio',
+    },
+
+    name: 'Albert Einstein',
+    birthdate: 'March 14, 1879',
+    birthplace:
+      'Ulm, Kingdom of Württemberg, German Empire',
+    deathdate: 'April 18, 1955',
+    deathplace: 'Princeton, New Jersey, United States',
+    nationality: 'Swiss, German, American',
+    occupation: 'Theoretical Physicist',
+    known_for: [
+      'Theory of Relativity',
+      'E=mc²',
+      'Photoelectric Effect',
+      'Brownian Motion',
+    ],
+    education: [
+      {
+        degree: 'Doctor of Philosophy',
+        field: 'Physics',
+        institution: 'University of Zurich',
+        year: 1905,
+      },
+    ],
+    notable_publications: [
+      {
+        title1: 'On the Electrodynamics of Moving Bodies',
+        year1: 1905,
+        publisher1: 'Annalen der Physik',
+      },
+      {
+        title2:
+          'Does the Inertia of a Body Depend Upon Its Energy Content?',
+        year2: 1905,
+        publisher2: 'Annalen der Physik',
+      },
+    ],
+    partners: [
+      {
+        name1: 'Mileva Marić',
+        relationship1: 'First wife',
+        years1: '1903-1919',
+      },
+      {
+        name2: 'Elsa Einstein',
+        relationship2:
+          "Second wife, also Einstein's first cousin",
+        years2: '1919-1936',
+      },
+    ],
+    awards: [
+      {
+        title: 'Nobel Prize in Physics',
+        year: 1921,
+        description:
+          'Awarded for his explanation of the photoelectric effect',
+      },
+    ],
+    influences: [
+      'Isaac Newton',
+      'James Clerk Maxwell',
+      'Hermann Minkowski',
+    ],
+    influenced: [
+      'Niels Bohr',
+      'Erwin Schrödinger',
+      'Werner Heisenberg',
+      'Richard Feynman',
+    ],
+    quotes: [
+      'Imagination is more important than knowledge.',
+      'I have no special talent. I am only passionately curious.',
+      'The important thing is not to stop questioning.',
+      'The most beautiful thing we can experience is the mysterious. It is the source of all true art and all science.',
+    ],
+    major_discoveries: [
+      {
+        title: 'Photoelectric Effect',
+        year: 1905,
+        description:
+          'Einstein explained the photoelectric effect, where electrons are emitted from a metal surface when it is illuminated by light.',
+      },
+    ],
+    contributions: {
+      title: 'Inventions',
+      description:
+        'Leonardo designed and invented a wide variety of machines and devices, including a helicopter, a diving suit, and a self-propelled cart. Many of his inventions were centuries ahead of their time.',
+      year: 'Late 15th to early 16th century',
+      field: 'Invention',
+    },
+  })
+})
